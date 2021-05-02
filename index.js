831877857921fcbe3b8443c13f18cf7fc917cc63
Dazz Granto <44319650+DazzGranto@users.noreply.github.com>
2021-05-02 15:23:22 +0300

    Remove TIME module

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 const TIME = 75e5;
-const MINUTE = 60e3;
+// const MINUTE = 60e3;
 const { Client } = require('discord.js');
-const moment = require('moment');
+//const moment = require('moment');
 const client = new Client();
 
 let channel;
-let time_channel;
-
-let days = {
-    'Monday'    : 'Pazartesi',
-    'Tuesday'   : 'Salı',
-    'Wednesday' : 'Çarşamba',
-    'Thursday' 	: 'Perşembe',
-    'Friday' 	: 'Cuma',
-    'Saturday' 	: 'Cumartesi',
-    'Sunday' 	: 'Pazar',
-};
+// let time_channel;
 
 client.on('ready', () => {
     channel = client.channels.get(process.env.CHANNEL_ID);
-    time_channel = client.channels.get(process.env.TIME_ID);
+    // time_channel = client.channels.get(process.env.TIME_ID);
 
     channel.send('!d bump').catch(e => console.log('FAILED: ', e));
 
-    client.channels.get(process.env.VOICE)
-    .join()
-    .catch(console.error);
+//     client.channels.get(process.env.VOICE)
+//     .join()
+//     .catch(console.error);
 });
 
 setInterval(() => {
     channel.send('!d bump').catch(e => console.log('FAILED: ', e));
 }, TIME);
 
-setInterval(() => {
-	let now = moment();
-	let dddd = now.format('dddd');
+// setInterval(() => {
+// 	let now = moment();
+// 	let dddd = now.format('dddd');
 
-	time_channel.setName(`Excited`);
-}, MINUTE);
+// 	time_channel.setName(`Excited`);
+// }, MINUTE);
 
 client.login(process.env.TOKEN);
